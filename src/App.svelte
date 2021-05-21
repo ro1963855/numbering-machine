ddaf1af4f1ec47d9f9a971b01c8792f1b67e7c88
Dandy <dandy.chen@buyandship.com.tw>
2021-05-22 01:19:05 +0800

    finish all feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,86 +1,112 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte'
+	import Counter from './components/Counter.svelte';
+	export let countersName = []
+	let counters = []
+	let totalNumber = 0
+	let waitingQueue = []
+
+	onMount(() => {
+		if (Array.isArray(counters)) {
+			counters = countersName.map(counterName => ({
+				name: counterName,
+				processing: null,
+				processed: [],
+			}))
+		}
+	})
+
+	$: {
+		handleDispatchJob(waitingQueue)
+	}
+
+	function handleDispatchJob() {
+		if (waitingQueue.length === 0) {
+			return
+		}
+
+		for (let index = 0; index < counters.length; index++) {
+			if (counters[index].processing === null) {
+				counters[index].processing = waitingQueue.shift()
+				counters = counters
+				waitingQueue = waitingQueue
+				return
+			}
+		}
+	}
+
+	function handleClickAddButton() {
+		totalNumber++
+		waitingQueue = [...waitingQueue, totalNumber]
+	}
+
+	function handleJobFinished(event) {
+		const counter = counters[event.detail.index]
+		counter.processed = [...counter.processed, counter.processing]
+		counter.processing = null
+		counters = counters
+		handleDispatchJob()
+	}
 </script>
 
 <main>
 	<header>
 		<h1>Bank Counter</h1>
 	</header>
+	<div class="nm__table__summary">
+		<span class="nm__table__summary__waiting">Waiting: { waitingQueue.length }</span>
+		<button class="nm__table__summary__add" on:click={handleClickAddButton}>Next {totalNumber + 1}</button>
+	</div>
 	<article class="nm__table">
 		<div class="nm__table__header">
 			<span class="nm__table__header__title">Counter</span>
 			<span class="nm__table__header__title">Processing</span>
 			<span class="nm__table__header__title">Processed</span>
 		</div>
-		<div class="nm__table__counter">
-			<span class="nm__table__counter__name">Amy</span>
-			<span class="nm__table__counter__processing">11</span>
-			<span class="nm__table__counter__processed">1,2,7</span>
-		</div>
-		<div class="nm__table__counter">
-			<span class="nm__table__counter__name">Bob</span>
-			<span class="nm__table__counter__processing">14</span>
-			<span class="nm__table__counter__processed">3,9</span>
-		</div>
-		<div class="nm__table__counter">
-			<span class="nm__table__counter__name">Cory</span>
-			<span class="nm__table__counter__processing">13</span>
-			<span class="nm__table__counter__processed">4,6,10</span>
-		</div>
-		<div class="nm__table__counter">
-			<span class="nm__table__counter__name">Dora</span>
-			<span class="nm__table__counter__processing">12</span>
-			<span class="nm__table__counter__processed">5,8</span>
-		</div>
+		{#each counters as counter, index}
+			<Counter
+				index="{index}"
+				name="{counter.name}"
+				processing="{counter.processing}"
+				processed="{counter.processed}"
+				on:jobFinished={handleJobFinished}
+			/>
+		{/each}
 	</article>
-	<div class="nm__table__summary">
-		<span class="nm__table__summary__waiting">18</span>
-		<button class="nm__table__summary__add">Next 33</button>
-	</div>
 </main>
 
 <style lang="scss">
+	* {
+		font-size: 1rem;
+		line-height: 1.5;
+	}
+
 	.nm__table {
 		display: table;
-		grid-template-columns: 1fr;
 
-		&__header, &__counter, &__summary {
+		&__header, &__summary {
 			display: table-row;
 		}
 
 		&__header {
 			&__title {
-				padding: 8px;
+				padding: 0.5rem;
 				display: table-cell;
 				width: min-content;
 				border-bottom: 1px solid gray;
-			}
-		}
-
-		&__counter {
-			&__name, &__processing, &__processed {
-				padding: 8px;
-				display: table-cell;
-				border-bottom: 1px solid gray;
-			}
-
-			&__name, &__processing {
-				display: table-cell;
-				width: min-content;
-				border-bottom: 1px solid gray;
-			}
-
-			&__processed {
-				width: 100%;
 			}
 		}
 
 		&__summary {
-			margin-top: 8px;
-			padding: 8px;
+			margin-top: 0.5rem;
+			padding: 0.5rem;
 			display: flex;
 			justify-content: space-between;
 			align-items: center;
+
+			&__add {
+				margin: 0;
+			}
 		}
 	}
 </style>
